bb000965aff4ea73cff999a5910f572a04c2068c
Srivas Chennu <srivas@gmail.com>
2013-01-29 00:00:15 +0000

    Checked in Tue Jan 29 00:00:15 GMT 2013
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Overnight@10 313b062d-9e61-4e68-82f3-f8d08bb62e75

---
+++ dataimport_chunk.m
@@ -0,0 +1,60 @@
+function dataimport_chunk(basename,startsec,endsec,chunkname)
+
+rawpath = '/Volumes/iStorage/Overnight/';
+filepath = '/Users/chennu/Data/Overnight/';
+
+filenames = dir(sprintf('%s%s*', rawpath, basename));
+
+if isempty(filenames)
+    error('No files found to import!\n');
+end
+
+mfffiles = filenames(logical(cell2mat({filenames.isdir})));
+if length(mfffiles) > 1
+    error('Expected 1 MFF recording file. Found %d.\n',length(mfffiles));
+end
+
+filename = mfffiles.name;
+fprintf('\nProcessing %s.\n\n', filename);
+
+mffjarpath = which('MFF-1.0.d0004.jar');
+javaaddpath(mffjarpath);
+hdr = read_mff_header(sprintf('%s%s', rawpath, filename),0);
+javarmpath(mffjarpath);
+
+EEG = pop_readegimff(sprintf('%s%s', rawpath, filename),...
+    'firstsample',startsec*hdr.Fs,'lastsample',endsec*hdr.Fs-1);
+    
+%%PREPROCESSING
+
+% REMOVE EXCLUDED CHANNELS
+chanexcl = [1,8,14,17,21,25,32,38,43,44,48,49,56,63,64,68,69,73,74,81,82,88,89,94,95,99,107,113,114,119,120,121,125,126,127,128];
+%chanexcl = [];
+fprintf('Removing excluded channels.\n');
+EEG = pop_select(EEG,'nochannel',chanexcl);
+
+%resample
+if EEG.srate > 250
+    EEG = pop_resample(EEG,250);
+end
+
+%Filter
+hpfreq = 0.5;
+lpfreq = 45;
+fprintf('Low-pass filtering below %.1fHz...\n',lpfreq);
+EEG = pop_eegfilt(EEG, 0, lpfreq, [], [0], 0, 0, 'fir1', 0);
+fprintf('High-pass filtering above %.1fHz...\n',hpfreq);
+EEG = pop_eegfilt(EEG, hpfreq, 0, [], [0], 0, 0, 'fir1', 0);
+
+%Remove line noise
+fprintf('Removing line noise at 50Hz.\n');
+EEG = rmlinenoisemt(EEG);
+
+EEG.setname = sprintf('%s_%s_orig',basename,chunkname);
+EEG.filename = sprintf('%s_%s_orig.set',basename,chunkname);
+EEG.filepath = filepath;
+
+EEG = eeg_checkset(EEG);
+
+fprintf('Saving %s%s.\n', EEG.filepath, EEG.filename);
+pop_saveset(EEG,'filename', EEG.filename, 'filepath', EEG.filepath);
