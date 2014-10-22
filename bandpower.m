0727f10d26b9ef5ac341d84a3b1fa0e284277302
Srivas Chennu <srivas@gmail.com>
2014-10-22 23:00:18 +0000

    Checked in Thu Oct 23 00:00:17 BST 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Overnight@18 313b062d-9e61-4e68-82f3-f8d08bb62e75

--- bandpower.m
+++ bandpower.m
@@ -9,5 +9,7 @@ else
     clear basename
 end
 
+EEG.spectra = 10.^(EEG.spectra/10);
+
 bpower = squeeze(mean(EEG.spectra(strcmp(channame,{EEG.chanlocs.labels}),...
     EEG.freqs >= freqwin(1) & EEG.freqs <= freqwin(2),:),2));
